4ad7a47500ec5dc557298ec17451adc81f1e4f54
Pranav Dembla <demblapranav@gmail.com>
2025-06-29 01:50:17 +0530

    scrren

--- carbon-footprint-client/src/common/PageWrapper.js
+++ carbon-footprint-client/src/common/PageWrapper.js
@@ -19,7 +19,7 @@ const PageWrapper = ({ children, backgroundImage }) => {
 
   return (
     <div
-      className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed md:no-scroll"
+      className="min-h-screen bg-cover bg-center bg-no-repeat md:bg-fixed bg-black transition-opacity duration-700 opacity-50"
       style={{
         backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
       }}
