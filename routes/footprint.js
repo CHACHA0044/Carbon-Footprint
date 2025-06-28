326f94068ca9198e10d099a1e83d38cbb8721382
Pranav Dembla <demblapranav@gmail.com>
2025-06-28 12:23:20 +0530

    authmiddleware fix

--- routes/footprint.js
+++ routes/footprint.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middleware/authMiddleware');
+const authenticateToken = require('../middleware/authmiddleware');
 const CarbonEntry = require('../models/CarbonEntry');
 
 // POST: Create entry
