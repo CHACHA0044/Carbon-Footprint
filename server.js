f1a772cfa0c4a67cef98a799d4c63b9a04589ba1
Pranav Dembla <demblapranav@gmail.com>
2025-06-28 23:27:36 +0530

    ser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 
 // ✅ Create Express app
 const app = express();
