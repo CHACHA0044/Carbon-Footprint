a68ba5ed538052b8fe87312c703e9e744c1fbf68
Pranav Dembla <demblapranav@gmail.com>
2025-07-17 21:24:03 +0530

    changed router to hash

--- carbon-footprint-client/src/App.js
+++ carbon-footprint-client/src/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import AnimatedRoutes from './Components/AnimatedRoutes';
 import { LoadingProvider } from 'context/LoadingContext';
 import React from 'react';
-//import PageWrapper from 'common/PageWrapper';
+
 function App() {
   return (
     <Router>
       <LoadingProvider>
-       {/* <PageWrapper>*/}
-          <AnimatedRoutes />
-       {/*  </PageWrapper>*/}
+        <AnimatedRoutes />
       </LoadingProvider>
     </Router>
   );
